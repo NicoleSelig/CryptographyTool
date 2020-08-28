e22372b3a4d88b2dfe8279331218746f7714b7b9
Nicole Selig <nselig@butler.edu>
2020-08-27 20:33:55 -0400

    more improvements to the menu

--- Main.java
+++ Main.java
@@ -1,22 +1,17 @@
-import java.io.*;
-import java.util.*;
 
 public class Main {
 
-public static void main(final String[] args) {
-    Menu menu = new Menu();
-    
-    System.out.println("Cryptography Tool");
-    System.out.println("By Nicole Selig");
-    System.out.println("");
+    public static void main(final String[] args) {
+        Menu menu = new Menu();
+        
+        System.out.println("Cryptography Tool");
+        System.out.println("By Nicole Selig");
+        System.out.println("");
 
-    menu.initCipherMenu();
-    Scanner scan = new Scanner(System.in);
-    String menuItem = scan.nextLine();
-    scan.close();
-    menu.chooseCipher(menuItem);
-    
-}
+        menu.initCipherMenu();
+        menu.chooseCipher();
+        
+    }
 
 }
 
--- Menu.java
+++ Menu.java
@@ -1,39 +1,99 @@
+import java.util.*;
 
 public class Menu {
 
-    String [] menuItems = {"Ceasar Cypher -- C", "Substitution Cypher -- S", "Home  -- H", "Quit -- Q"};
-   
+    String [] cipherMenuItems = {"Ceasar Cypher -- C", "Substitution Cypher -- S", "Home  -- H", "Quit -- Q"};
+    String [] cryptMenuItems = {"Encrypt -- E"," Decrypt -- D", "Home -- H", "Quit -- Q"};
+    
     //presents the Cipher Menu
     public void initCipherMenu(){
+        System.out.println("");
         System.out.println("Which Cypher did you want to use?");
         System.out.println("");
 
-        for (int i = 0; i <= menuItems.length - 1; i++){
-            System.out.println(menuItems[i]);
+        for (int i = 0; i <= cipherMenuItems.length - 1; i++){
+            System.out.println(cipherMenuItems[i]);
+        }
+    }
+    
+    //presents the crypt menu
+    public void initCryptMenu(){
+        System.out.println("");
+        System.out.println("Encryption or Decryption?");
+        System.out.println("");
+
+        for (int i = 0; i <= cryptMenuItems.length - 1; i++){
+            System.out.println(cryptMenuItems[i]);
         }
     }
 
     //Recieves user's menu choice and picks a Cipher
-    public void chooseCipher(String menuItem) {
+    public void chooseCipher() {
+        Scanner scan = new Scanner(System.in);
+        String menuItem = scan.next();
         switch(menuItem) {
             case "c":
             case "C":
                 System.out.println("Ceasar Cipher");
+                initCryptMenu();
+                chooseCrypt();
                 break;
             case "s":
             case "S":
                 System.out.println("Substitution Cipher");
+                initCryptMenu();
+                chooseCrypt();
                 break;
             case "q":
             case "Q":
                 System.out.println("Quit");
+                System.out.println("Good Bye!");
                 System.exit(0);
                 break;
             case "h":
             case "H":
+                System.out.println("Home");
                 initCipherMenu();
+                chooseCipher();
+                break;
             default:
                 System.out.println("Invalid Choice. Try Again");
+                chooseCipher();
+                break;
+        }  
+    }
+    
+    //recieves the users choice chooses a crypt
+    public void chooseCrypt() {
+        Scanner scan = new Scanner(System.in);
+        String menuItem = scan.next();
+        switch(menuItem) {
+            case "e":
+            case "E":
+                System.out.println("Encrypt");
+                scan.close();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Decrypt");
+                scan.close();
+                break;
+            case "q":
+            case "Q":
+                System.out.println("Quit");
+                scan.close();
+                System.exit(0);
+                break;
+            case "h":
+            case "H":
+                System.out.println("Home");
+                initCipherMenu();
+                chooseCipher();
+                break;
+            default:
+                System.out.println("Invalid Choice. Try Again");
+                chooseCrypt();
+                break;
         }  
     }
 }
